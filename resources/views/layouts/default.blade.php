0879b9c83badd07e9f6d3cf7feed6bfdb2afd6a0
ymrS <yumiruSky@gmail.com>
2018-03-13 18:39:09 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,13 @@
 <html>
 <head>
     <title>@yield('title','Default')</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+	@include('layouts._header')
+	<div class="container">
     @yield('content')
+    </div>
+	@include('layouts._footer')
 </body>
 </html>
