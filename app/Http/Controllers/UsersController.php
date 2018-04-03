4a017b81697a27557eff6d77761ea8ef94003d52
ymrS <yumiruSky@gmail.com>
2018-04-03 04:30:20 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -29,8 +30,9 @@ class UsersController extends Controller
 			'password'=>bcrypt($request->password),
 		]);
 		
+		Auth::login($user);
 		session()->flash('success','注册成功!');
 		return redirect()->route('users.show',[$user]);
-	}
+	}	
 	
 }
